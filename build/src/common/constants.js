1b9c14e31ff56e81235e5a0024d86e88bf579460
Udit-Kapoor <udit.dude01@gmail.com>
2022-08-28 04:02:57 +0530

    feat: add config

--- build/src/common/constants.js
+++ build/src/common/constants.js
@@ -3,3 +3,24 @@ export const rpcNode = "https://ghostnet.smartpy.io";
 
 
 export const teztrisContract = 'KT1KY1nnwawbqyXz2g2b9tS7qCaiEidnkZWb';
+
+export const ctez = {
+    betToken : "KT1Q4qRd8mKS7eWUgTfJzCN8RC6h9CzzjVJb",
+    betTokenId: 0,
+    betTokenType : "FA1.2",
+    betTokenDecimal : 6,
+}
+
+export const usdt = {
+    betToken : "KT1Uw1oio434UoWFuZTNKFgt5wTM9tfuf7m7",
+    betTokenId: 3,
+    betTokenType : "FA2",
+    betTokenDecimal : 6,
+}
+
+export const tez = {
+    betToken : "",
+    betTokenId: 0,
+    betTokenType : "tez",
+    betTokenDecimal : 6,
+}
