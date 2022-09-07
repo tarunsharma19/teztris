99e580b0bbcbbd83185705b55be64c6b194e3b4a
Udit-Kapoor <udit.dude01@gmail.com>
2022-09-08 01:49:48 +0530

    feat: shift to azure backend

--- build/src/api/socket.js
+++ build/src/api/socket.js
@@ -1,8 +1,12 @@
 import io from "socket.io-client";
 
-const URL = "https://teztris-backend.herokuapp.com/";
+// const URL = "https://teztris-backend.herokuapp.com/";
 
-//const URL = "localhost:3000";
+// const URL = "https://teztris.azurewebsites.net:3000";
+
+const URL = "http://20.204.28.110:8080";
+
+// const URL = "localhost:8080";
 
 const socket = io(URL, { transports: ["websocket"] });
 
