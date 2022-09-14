81cd7ea1215a708e8b447b5b84db6d9321e885c1
Udit-Kapoor <68367301+Udit-Kapoor@users.noreply.github.com>
2022-09-15 01:23:38 +0530

    Add files via upload

---
+++ nft.js
@@ -0,0 +1,130 @@
+
+// const { PDFDocument, rgb, degrees } = PDFLib;
+
+const PDFLib = require("pdf-lib");
+const pinata = require("./pinata");
+var fs = require('fs');
+var PDFImage = require("pdf-image2").PDFImage;
+
+
+const nftFlow = async(player1 , player2 , amount , token) =>{
+
+    try {
+        console.log('inside nft flow');
+
+        generatePDF(player1 , player2 , amount , token);
+
+        // get file
+        // let img = fs.createReadStream('./trial.pdf');
+
+        var pdfImage = new PDFImage("./trial.pdf");
+        const img = await pdfImage.convertPage(0);
+        console.log(img);
+
+
+        // Only error is upload file to ipfs
+
+        const res = await pinata.sendFileToIPFS();
+        console.log(res);
+
+        const res2 = await pinata.pinataWrapper(player1 , player2 , amount+" "+token , res.Ipfs);
+        console.log(res2);
+
+        return{
+            success : true,
+            Ipfs : res2.Ipfs
+        };
+
+
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const generatePDF = async (player1 , player2 , amount , token) => {
+
+    try {
+        console.log('inside generate pdf');
+        let existingPdfBytes;
+        var text = fs.readFileSync('./template.pdf');
+        // console.log(text);
+        existingPdfBytes = text;
+        console.log(existingPdfBytes);
+    
+      // Load a PDFDocument from the existing PDF bytes
+      const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
+    
+      const Poppins = await pdfDoc.embedFont(PDFLib.StandardFonts.HelveticaBold);
+    
+      // Get the first page of the document
+      const pages = pdfDoc.getPages();
+      const firstPage = pages[0];
+    
+      // Draw a string of text diagonally across the first page
+
+        let top = player1.substring(0,4) + "...." + player1.substring(player1.length-4,player1.length);
+
+        let bottom = player2.substring(0,4)+"...."+player2.substring(player2.length-4,player2.length);
+
+      firstPage.drawText(top, {
+        x: 310,
+        y: 470,
+        size: 22,
+        font: Poppins,
+        color: PDFLib.rgb(1, 1, 1),
+      });
+    
+      firstPage.drawText(bottom, {
+        x: 310,
+        y: 415,
+        size: 22,
+        font: Poppins,
+        color: PDFLib.rgb(1, 1, 1),
+      });
+    
+      firstPage.drawText(token +" "+ amount, {
+        x: 350,
+        y: 337,
+        size: 15,
+        font: Poppins,
+        color: PDFLib.rgb(1, 1, 1),
+      });
+    
+    //   firstPage.drawText(token, {
+    //     x: 400,
+    //     y: 340,
+    //     size: 12,
+    //     // font: Poppins,
+    //     color: PDFLib.rgb(1, 1, 1),
+    //   });
+    
+      // Serialize the PDFDocument to bytes (a Uint8Array)
+      const pdfBytes = await pdfDoc.save();
+      
+    
+      // this was for creating uri and showing in iframe
+    
+      const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    
+      // document.getElementById("pdf").src = pdfDataUri;
+    
+      fs.writeFileSync('./trial.pdf', pdfBytes);
+      console.log("Done creating");
+    
+      return{
+          success : true
+      };
+    
+        
+    } catch (error) {
+        console.log(error);
+        return{success : false};
+    }
+    
+   
+
+};
+
+module.exports = {nftFlow};
---
+++ pinata.js
@@ -0,0 +1,135 @@
+const axios = require("axios");
+var FormData = require('form-data');
+var fs = require('fs');
+
+const jsonToPinata = async (json) =>  {
+
+    console.log('inside JSON to pinata');
+    try {
+
+        const data = JSON.stringify(json);
+
+        var config = {
+            method: 'post',
+            url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+            headers: { 
+              'Content-Type': 'application/json', 
+              pinata_api_key: 'c98008ccad0a8398468f',
+              pinata_secret_api_key: 'b925e8a94f1aa1dbb8bd85a376ac527847daa4d3bd3a37aaa2e84e83c905d19e',
+            },
+            data : data
+          };
+          
+          const res = await axios(config);
+        return{
+            sucess : true,
+            Ipfs : res.data.IpfsHash,
+        };
+
+    } catch (error) {
+        console.log(error);
+        return{
+            sucess:false,
+            Ipfs : "",
+        };
+        
+    }
+
+}
+
+const sendFileToIPFS = async () => {
+
+    console.log('inside send file');
+
+        try {
+
+            const fileImg = fs.createReadStream('./trial-0.png');
+
+            const formData = new FormData();
+            formData.append("file", fileImg);
+
+            const resFile = await axios({
+                method: "post",
+                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+                data: formData,
+                headers: {
+                    pinata_api_key: 'c98008ccad0a8398468f',
+                    pinata_secret_api_key: 'b925e8a94f1aa1dbb8bd85a376ac527847daa4d3bd3a37aaa2e84e83c905d19e',
+                    "Content-Type": "multipart/form-data"
+                },
+            });
+
+            const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+         console.log(ImgHash); 
+
+         return{
+            sucess : true,
+            Ipfs : ImgHash,
+        };
+
+        } catch (error) {
+            console.log("Error sending File to IPFS: ")
+            console.log(error)
+            return{
+                sucess : false,
+                Ipfs : "",
+            };
+        }
+    }
+
+const pinataWrapper = async (player1 , player2 , amount, image) =>  {
+
+    console.log('inside pinata wrapper');
+
+    try {
+        let data = {};
+
+            data.name = "TezTris NFT";
+            data.description = "This NFT certifies that you have won a game of TezTris! Have fun flexing it!";
+            data.artifactUri = image;
+            data.displayUri = image;
+            data.thumbnailUri = image;
+            data.decimals = 0 ;
+
+            data.attributes = [
+              {
+                "name": "Winner",
+                "value": player1,
+              },
+              {
+                "name": "Loser",
+                "value": player2,
+              }
+              ,
+              {
+                "name": "Amount",
+                "value": amount,
+              }
+            ];
+            
+            data.creators =  [
+                "tz1NaGu7EisUCyfJpB16ktNxgSqpuMo8aSEk"
+              ];
+            data.isBooleanAmount = false;
+              data.symbol = "TRIZ";
+              data.rights =  "All right reserved.";
+              data.shouldPreferSymbol = true;
+
+        const res = await jsonToPinata(data);
+        return{
+            sucess : true,
+            Ipfs : res.Ipfs,
+        };
+
+    } catch (error) {
+        console.log(error);
+        return{
+            sucess:false,
+            Ipfs : "",
+        };
+        
+    }
+
+}
+
+module.exports = {sendFileToIPFS , pinataWrapper};
