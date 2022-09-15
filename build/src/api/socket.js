dd8be1ee412ddb68169d7abd1e10397afa64a854
Udit-Kapoor <udit.dude01@gmail.com>
2022-09-15 19:17:10 +0530

    feat: shift to azure

--- build/src/api/socket.js
+++ build/src/api/socket.js
@@ -1,10 +1,10 @@
 import io from "socket.io-client";
 
- const URL = "https://teztris-backend.herokuapp.com/";
+//  const URL = "https://teztris-backend.herokuapp.com/";
 
 // const URL = "https://teztris.azurewebsites.net:3000";
 
-//const URL = "http://20.204.28.110:8080";
+const URL = "http://20.204.28.110:8080";
 
 // const URL = "localhost:8080";
 
