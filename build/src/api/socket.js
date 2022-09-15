355ca69614887a23e7d97f2a2b74a399b114b2e0
Udit-Kapoor <udit.dude01@gmail.com>
2022-09-16 01:48:11 +0530

    backend https

--- build/src/api/socket.js
+++ build/src/api/socket.js
@@ -3,14 +3,14 @@ import io from "socket.io-client";
 //  const URL = "https://teztris-backend.herokuapp.com/";
 
 
-// const URL = "http://20.204.28.110:8080";
+// const URL = "http://20.204.28.110";
 
-const URL = `/api`;
+// const URL = `/api`;
 
 
 // const URL = "http://teztris.centralindia.cloudapp.azure.com:8080";
 
-// const URL = "https://minfy.live";
+const URL = "https://minfy.live";
 
 // const URL = "localhost:8080";
 
