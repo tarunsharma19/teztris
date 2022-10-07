a8277d647c1cf36794ba3235d9717a033b5b56ad
TarunWebDev <sharmatarun1019@gmail.com>
2022-10-08 00:24:55 +0530

    fix: scoring on line clear

--- build/src/hooks/useGameStatus.js
+++ build/src/hooks/useGameStatus.js
@@ -5,7 +5,7 @@ export const useGameStatus = rowsCleared => {
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  const linePoints = [40, 100, 300, 1200];
+  const linePoints = [40, 100, 400, 1000, 1400];
 
   const calcScore = useCallback(() => {
     // We have score
