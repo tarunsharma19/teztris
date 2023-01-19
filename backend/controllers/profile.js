219c99f15ec888e31465aef90b91060ece128afb
Ayush Garg <ayush181000@gmail.com>
2023-01-19 05:47:48 +0530

    add pipelines

--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -2,6 +2,7 @@ const catchAsync = require('../util/catchAsync');
 const Game = require('../models/gameModel');
 const User = require('../models/userModel');
 
+// NOTE: can be divided into multiple parts according to front end
 exports.getMe = catchAsync(async (req, res, next) => {
     if (!req.query.id) {
         throw new Error('Id not found');
@@ -11,10 +12,55 @@ exports.getMe = catchAsync(async (req, res, next) => {
     const me = await User.findById(myWalletId);
     const myGames = await Game.find({ $or: [{ me: myWalletId }, { opponent: myWalletId }] });
 
+    const winnerPipeline = [
+        {
+            '$match': {
+                'winner': myWalletId
+            }
+        }, {
+            '$group': {
+                '_id': '$tokenData.betToken',
+                'totalAmountWon': {
+                    '$sum': '$tokenData.amount'
+                }
+            }
+        }
+    ];
+
+    const loserPipeline = [
+        {
+            '$match': {
+                '$or': [
+                    {
+                        'me': myWalletId
+                    }, {
+                        'opponent': myWalletId
+                    }
+                ],
+                'winner': {
+                    '$ne': myWalletId
+                }
+            }
+        }, {
+            '$group': {
+                '_id': '$tokenData.betToken',
+                'totalAmountLost': {
+                    '$sum': '$tokenData.amount'
+                }
+            }
+        }
+    ]
+
+    const winnings = await Game.aggregate(winnerPipeline);
+    const loosings = await Game.aggregate(loserPipeline);
+
     res.send({
         status: 200,
         message: 'Fetched leaderboard successfully',
         me,
-        myGames
+        myGames,
+        winnings,
+        loosings
     })
 });
+
