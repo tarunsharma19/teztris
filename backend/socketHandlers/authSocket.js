d967c84bbcfe2e61a9c0862904d56701f34e49ad
Ayush Garg <ayush181000@gmail.com>
2023-02-01 23:50:51 +0530

    .

--- backend/socketHandlers/authSocket.js
+++ backend/socketHandlers/authSocket.js
@@ -2,6 +2,7 @@
 const verifyTokenSocket = (socket, next) => {
   try {
     const wallet = socket.handshake.headers?.auth;
+    console.log('someone trying to connect');
     if (!wallet) {
       throw new Error('Please provide wallet id');
     }
