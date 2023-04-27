ac22c81cd7d3bea1a9811cc90881225459c90daf
Tarun Sharma <sharmatarun1019@gmail.com>
2023-04-27 12:10:59 +0530

    feat: profile fetch

--- build/src/components/profile/Profile.js
+++ build/src/components/profile/Profile.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './scss/Profile.scss';
 import UserProfile from './UserProfile';
 import NFTs from './NFTs';
 import MatchHistory from './MatchHistory';
 import Navbar from '../dashboard/Navbar';
+import axios from "axios";
+import {manageFunc} from '../../App'
 
 const Profile = () => {
+  const { userWallet } = useContext(manageFunc);
+
   // Add your data for each component here
+  const[profile,setProfile] = useState([]);
+  const getProfile = async (wallet) =>{
+      const res = await axios.get(
+          `localhost:8080/api/profile?id=${wallet}`
+        );
+      const data = res.data.users; 
+      console.log("data",data)
+      // const formattedResponse = data.map(player => ({
+      //     address: player._id,
+      //     wins: player.won,
+      //     lose: player.lost,
+      //     highScore: player.highScore,
+      //   }))
+      // return formattedResponse;
+  }
+
+  getProfile(userWallet);
+
+
   const userProfileData = {
     profileImage: 'https://placehold.jp/800x800.png',
     userName: 'John Doe',
@@ -77,9 +100,9 @@ const Profile = () => {
   return (
     <div className="app">
         <Navbar />
-      <UserProfile {...userProfileData} />
-      <NFTs nfts={nftGalleryData} />
-      <MatchHistory matches={matchHistoryData} />
+        <UserProfile {...userProfileData} />
+        <NFTs nfts={nftGalleryData} />
+        <MatchHistory matches={matchHistoryData} />
     </div>
   );
 }
